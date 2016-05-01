e3c51ebd69ee65137df2bf64b5c5a76d3bbbbe92
Greg McIntyre <nocolong@gmail.com>
2016-04-30 19:09:10 -0700

    Using ContainsKey, ArrayList, and format..

--- JiuJitsuHashTable/JiuJitsuHashTable/Program.cs
+++ JiuJitsuHashTable/JiuJitsuHashTable/Program.cs
@@ -11,6 +11,8 @@ namespace JiuJitsuHashTable
     {
         static void Main()
         {
+            //new instance of a hashtable class,
+            //then added key value pairs to it.
             Hashtable ht = new Hashtable();
             ht.Add("Greg McIntyre", "Black Belt");
             ht.Add("Jade Charles", "Blue Belt");
@@ -28,9 +30,30 @@ namespace JiuJitsuHashTable
             ht.Add("Brent Wirth", "Brown Belt");
             ht.Add("Dae Li", "White Belt");
 
+            //Using DictionaryEntry to loop through all entries in the table.
             foreach (DictionaryEntry e in ht)
             {
-                Console.WriteLine(e.Key.ToString(), e.Value);
+                Console.WriteLine("{0}, {1}", e.Key, e.Value);
+            }
+            Console.WriteLine();
+            //if the table doesn't contain key Kyla McIntyre, add it to the table.
+            if (!ht.ContainsKey("Kyla McIntyre"))
+            {
+                ht.Add("Kyla McIntyre", "Pink Belt");
+                //Prints the value of the added key.
+                Console.WriteLine("Belt value added for Kyla McIntyre : {0}", ht["Kyla McIntyre"]);
+            }
+            Console.WriteLine();
+            //look up the value of key Greg McIntyre
+            string value = (string)ht["Greg McIntyre"];
+            Console.WriteLine("Belt value for Greg McIntyre is : {0}", value);
+
+            Console.WriteLine();
+            //Using ArrayList to store the keys.
+            ArrayList aL = new ArrayList(ht.Keys);
+            foreach (string key in aL)
+            {
+                Console.WriteLine(key);
             }
             Console.ReadLine();
         }
